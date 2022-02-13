b0b26582b2b8e9ac1abebd6b753e4c0311a8c42d
OliEfr <olli.haus97@gmail.com>
2022-02-13 09:28:17 +0100

    moved availableFor section to project information

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,9 +92,9 @@ const Home: NextPage = () => {
           <h2>{t('index.subHeading')}</h2>
           <h2 style={{marginBottom: 0}}>{t('index.installPrompt')} <Link href={browser.url}>{browser.name}</Link> <img src={browser.icon} alt="" loading="lazy" className={styles.browserIcon}/></h2>
 
-          <ReactMarkdown className={styles.availableFor} children={t('index.availableFor', {firefox: browserList.firefox.url, chrome: browserList.chrome.url})} components={{
+          {/* <ReactMarkdown className={styles.availableFor} children={t('index.availableFor', {firefox: browserList.firefox.url, chrome: browserList.chrome.url})} components={{
             a: ({href, children}) => <Link href={href || ''}>{children[0] || ''}</Link>
-          }}/>
+          }}/> */}
         </div>
 
         <div className={styles.section}>
